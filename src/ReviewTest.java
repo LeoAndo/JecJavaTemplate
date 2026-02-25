3fe0d2d79bcbdfbd40aedc7297b89686debf5953
LeoAndo <leo.777.info@gmail.com>
2026-02-25 22:31:28 +0900

    Add `ReviewTest` class with a basic main method

---
+++ src/ReviewTest.java
@@ -0,0 +1,9 @@
+public class ReviewTest {
+    static void main() {
+        String name = "Claude";
+        if(true){
+            name = "Codex";
+        }
+        System.out.println(name);
+    }
+}
